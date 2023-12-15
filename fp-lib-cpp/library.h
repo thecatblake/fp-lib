f776d29cf5c66b16c2e47525f5f2f5204e23a871
thecatblake <k222ryousuke@gmail.com>
2023-12-15 16:30:43 +0900

    add memoise

--- fp-lib-cpp/library.h
+++ fp-lib-cpp/library.h
@@ -1,17 +1,33 @@
 #ifndef FP_LIB_CPP_LIBRARY_H
 #define FP_LIB_CPP_LIBRARY_H
 
-#include "functional"
-#include "utility"
+#include <functional>
+#include <utility>
+#include <unordered_map>
 
 template<typename T>
 constexpr T id(T t) noexcept {
-    return std::forward<T>(t);
+    return t;
 }
 
 template<typename A, typename B, typename C>
 std::function<C(A)> compose(B (*f)(A), C (*g)(B)) {
     return [f,g](A x) { return g(f(x)); };
 }
+
+template<typename A, typename B>
+struct memoise {
+    std::unordered_map<A, B> mem;
+
+    std::function<B(A)> operator()(B (*f)(A)) {
+        return [f, this](A x) {
+
+            if(this->mem.find(x) != this->mem.end()) return this->mem[x];
+
+            this->mem[x] = f(x);
+            return this->mem[x];
+        };
+    }
+};
 
 #endif //FP_LIB_CPP_LIBRARY_H
--- fp-lib-cpp/play_test.cpp
+++ fp-lib-cpp/play_test.cpp
@@ -3,7 +3,8 @@
 //
 
 #include <iostream>
-#include <cmath>
+#include <chrono>
+using namespace std::chrono;
 #include "library.h"
 
 int triple(int x) {
@@ -14,8 +15,31 @@ double half(int x) {
     return x / 2.0;
 }
 
+int fib(int x) {
+    if(x <= 2) return 1;
+    return fib(x - 2) + fib(x - 1);
+}
+
 int main() {
-    int x = 1;
-    auto h = compose(id<int>, triple);
-    std::cout << h(x) << std::endl;
+   memoise<int, int> mem;
+   auto fib2 = mem(fib);
+   auto start = high_resolution_clock::now();
+
+   for(int i=0; i < 1000; i++) {
+       fib2(30);
+   }
+
+   auto stop = high_resolution_clock::now();
+   auto duration = duration_cast<microseconds>(stop - start);
+   std::cout << duration.count() << std::endl;
+
+   start = high_resolution_clock::now();
+
+    for(int i=0; i < 1000; i++) {
+        fib(30);
+    }
+
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    std::cout << duration.count() << std::endl;
 }
